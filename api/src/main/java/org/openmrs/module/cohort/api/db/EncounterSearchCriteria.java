383e476845626bf1e4e136b6cae74bd4144e14c2
corneliouzbett <kbett68@gmail.com>
2021-08-19 12:37:17 +0300

    Harmonize license headers

--- api/src/main/java/org/openmrs/module/cohort/api/db/EncounterSearchCriteria.java
+++ api/src/main/java/org/openmrs/module/cohort/api/db/EncounterSearchCriteria.java
@@ -1,6 +1,4 @@
-package org.openmrs.module.cohort.api.db;
-
-/**
+/*
  * This Source Code Form is subject to the terms of the Mozilla Public License,
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
@@ -9,6 +7,8 @@ package org.openmrs.module.cohort.api.db;
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
+package org.openmrs.module.cohort.api.db;
+
 import java.util.Collection;
 import java.util.Date;
 
