47b0e48a8cd129e6050bfdca6f9098983918f682
Luis Oliveira <luis.oliveira@icrc.org>
2023-08-02 11:31:05 +0100

    CohortResourceTest - Added Cohort attribute tests

--- omod/src/test/java/org/openmrs/module/cohort/web/resource/CohortResourceTest.java
+++ omod/src/test/java/org/openmrs/module/cohort/web/resource/CohortResourceTest.java
@@ -13,6 +13,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.cohort.CohortAttribute;
+import org.openmrs.module.cohort.CohortAttributeType;
 import org.openmrs.module.cohort.CohortM;
 import org.openmrs.module.cohort.api.CohortService;
 import org.openmrs.module.webservices.rest.web.RequestContext;
@@ -30,6 +32,14 @@ public class CohortResourceTest extends BaseCohortResourceTest<CohortM, CohortRe
 	
 	private static final String COHORT_NAME = "Test cohort attribute type";
 	
+	private static final String COHORT_ATTRIBUTE_TYPE_UUID = "09790099-9190-429d-811a-aac9edb8d98e";
+	
+	private static final String COHORT_ATTRIBUTE_TYPE_NAME = "Test";
+	
+	private static final String COHORT_ATTRIBUTE_TYPE_DESCRIPTION = "This is a test group.";
+	
+	private static final String COHORT_ATTRIBUTE_TYPE_DATATYPE = "org.openmrs.customdatatype.datatype.FreeTextDatatype";
+	
 	@Mock
 	@Qualifier("cohort.cohortService")
 	private CohortService cohortService;
@@ -41,6 +51,20 @@ public class CohortResourceTest extends BaseCohortResourceTest<CohortM, CohortRe
 		cohort = new CohortM();
 		cohort.setUuid(COHORT_UUID);
 		cohort.setName(COHORT_NAME);
+		CohortAttributeType attributeType = new CohortAttributeType();
+		attributeType.setUuid(COHORT_ATTRIBUTE_TYPE_UUID);
+		attributeType.setName(COHORT_ATTRIBUTE_TYPE_NAME);
+		attributeType.setDescription(COHORT_ATTRIBUTE_TYPE_DESCRIPTION);
+		attributeType.setMinOccurs(0);
+		attributeType.setDatatypeClassname(COHORT_ATTRIBUTE_TYPE_DATATYPE);
+		CohortAttribute attribute1 = new CohortAttribute();
+		attribute1.setAttributeType(attributeType);
+		attribute1.setValue("Test group");
+		cohort.addAttribute(attribute1);
+		CohortAttribute attribute2 = new CohortAttribute();
+		attribute2.setAttributeType(attributeType);
+		attribute2.setValue("Control group");
+		cohort.addAttribute(attribute2);
 		
 		//Mocks
 		this.prepareMocks();
@@ -86,6 +110,7 @@ public class CohortResourceTest extends BaseCohortResourceTest<CohortM, CohortRe
 		assertThat(result, notNullValue());
 		assertThat(result.getUuid(), is(COHORT_UUID));
 		assertThat(result.getName(), is(COHORT_NAME));
+		assertThat(result.getAttributes().size(), is(2));
 	}
 	
 	@Test
@@ -97,6 +122,7 @@ public class CohortResourceTest extends BaseCohortResourceTest<CohortM, CohortRe
 		assertThat(newlyCreatedObject, notNullValue());
 		assertThat(newlyCreatedObject.getUuid(), is(COHORT_UUID));
 		assertThat(newlyCreatedObject.getName(), is(COHORT_NAME));
+		assertThat(newlyCreatedObject.getAttributes().size(), is(2));
 	}
 	
 	@Test
